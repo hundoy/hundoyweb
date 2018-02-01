7230a07d709581d2edb4ca34a63658a42463da69
wanggyd@chanjet.com <wanggyd@chanjet.com>
2018-02-01 15:49:20 +0800

    change controller

--- src/main/java/com/hundoy/hundo/CommonController.java
+++ src/main/java/com/hundoy/hundo/CommonController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,5 +18,19 @@ public class CommonController
     public String index()
     {
         return "index";
+    }
+
+    @RequestMapping(value = "/getauth", method = RequestMethod.GET)
+    @ResponseBody
+    public String getauth()
+    {
+        return "getauth";
+    }
+
+    @RequestMapping(value = "/cancelauth", method = RequestMethod.GET)
+    @ResponseBody
+    public String cancelauth()
+    {
+        return "cancelauth";
     }
 }
